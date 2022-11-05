12f0b6196ca9599f34b98970a6556166bf3cc2c3
LiyanNguyen <liyannguyenn@gmail.com>
2022-11-05 21:07:43 +0800

    Update script.js
    - Added delete a todo list item functionality

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+/*
+NOTE TO SELF:
+if you are looking at this code in the future
+do NOT bother remembering how this works
+doing this in vanilla JS was a HORRIBLE IDEA
+do it in a framework instead!
+
+GOOD JOB THO! this was a fun challenge
+but not the most optimized way of doing things
+*/
+
 let newTodoInput = document.querySelector("#newTodoInput");
 let currentListDisplay = document.querySelector("#currentList");
 let viewAllTodos = document.querySelector("#allTodos");
@@ -59,17 +70,53 @@ newTodoInput.onkeydown = (event) => {
 		currentListDisplay.appendChild(li);
 		newTodoInput.value = "";
 		addCheckBoxFunction();
+		addDeleteTodoItemFunction();
 		// update the number of active todo items left
 		todoItemsLeft.innerText = `${activeTodo.length} items left`;
 	}
 }
+
+// states of wheter which to do list view type is selected
+let allIsSelected = true;
+let activeIsSelected = false;
+let completedIsSelected = false;
+
+// DELETE A TODO FUNCTION
+let addDeleteTodoItemFunction = () => {
+  let deleteTodoIcon = document.querySelectorAll("#currentList li img");
+  deleteTodoIcon.forEach((e) => {
+    e.onclick = () => {
+      // find out the item from that todo item
+      // then search from the active todo based on the value
+      let indexToDelete = activeTodo.indexOf(e.previousSibling.textContent);
+      // then delete it
+      activeTodo.splice(indexToDelete, 1);
+      // update allTodoData array as well (this is easier with framework)
+      let allTodoData = completedTodo.concat(activeTodo);
+      // update the number of active todo items left
+      todoItemsLeft.innerText = `${activeTodo.length} items left`;
+      // then update the UI to with the removed active todo item
+      currentListDisplay.innerHTML = "";
+      if (allIsSelected == true) {
+        displayCompletedTodos();
+        displayActiveTodos();
+        addCheckBoxFunction();
+      } else if (activeIsSelected == true) {
+        displayActiveTodos();
+        addCheckBoxFunction();
+      }
+    };
+  });
+};
+
+addDeleteTodoItemFunction();
 
 // RENDER COMPLETED TODOS
 let displayCompletedTodos = () => {
 	completedTodo.forEach((data) => {
 		let completedList = document.createElement("li");
 		completedList.classList.add("completed");
-		completedList.innerHTML = `<div class="checkBox"></div>${data}<img src="images/icon-cross.svg" alt="x icon">`;
+		completedList.innerHTML = `<div class="checkBox"></div>${data}`;
 		currentListDisplay.appendChild(completedList);
 	})
 }
@@ -80,6 +127,7 @@ let displayActiveTodos = () => {
 		let activeList = document.createElement("li");
 		activeList.innerHTML = `<div class="checkBox"></div>${data}<img src="images/icon-cross.svg" alt="x icon">`;
 		currentListDisplay.appendChild(activeList);
+		addDeleteTodoItemFunction();
 	});
 }
 
@@ -92,6 +140,9 @@ viewAllTodos.onclick = () => {
 	viewAllTodos.classList.add("active")
 	viewActiveTodos.classList.remove("active");
 	viewCompletedTodos.classList.remove("active");
+	allIsSelected = true;
+  activeIsSelected = false;
+  completedIsSelected = false;
 }
 
 // VIEW ACTIVE TODOS FUNCTION
@@ -102,6 +153,9 @@ viewActiveTodos.onclick = () => {
 	viewAllTodos.classList.remove("active");
 	viewActiveTodos.classList.add("active");
 	viewCompletedTodos.classList.remove("active");
+	activeIsSelected = true;
+	completedIsSelected = false;
+	allIsSelected = false;
 };
 
 // VIEW COMPLETED TODOS FUNCTION
@@ -111,6 +165,9 @@ viewCompletedTodos.onclick = () => {
 	viewAllTodos.classList.remove("active");
 	viewActiveTodos.classList.remove("active");
 	viewCompletedTodos.classList.add("active");
+	activeIsSelected = false;
+  completedIsSelected = true;
+  allIsSelected = false;
 };
 
 // CLEAR THE CURRENT COMPLETED TODOS AND RENDER ALL THE ACTIVE TODOS
@@ -129,24 +186,6 @@ themeSwitcher.onclick = () => {
 	alert("This Feature is still WIP")
 }
 
-// DELETE A TODO FUNCTION (WIP)
-let deleteTodoIcon = document.querySelectorAll("#currentList li img");
-deleteTodoIcon.forEach((e) => {
-	e.onclick = () => {
-    // find out the item from that todo item
-    // then search from the active todo based on the value
-    let indexToDelete = activeTodo.indexOf(e.previousSibling.textContent);
-    // then delete it
-		activeTodo.splice(indexToDelete, 1);
-		// update allTodoData array as well (this is easier with framework)
-		let allTodoData = completedTodo.concat(activeTodo);
-		console.log(allTodoData)
-		// then update the UI to with the removed active todo item
-
-    // try to console log allTodoData array, see if it updates as well
-  }
-})
-
 /*
 NEW LEARNINGS: 
 nextSibling
@@ -155,6 +194,7 @@ previousElementSibling
 nextElementSibling
 parentElement
 cloneNode + replaceWith (TRICK TO REMOVE EVENTLISTENERS FROM AN ELEMENT)
+THIS IS HORRIBLE IN VANILLA, DO IT IN A FRAMEWORK, SO MUCH CODE REPITITION LMAO
 */
 
 // BONUS DIFFICULT FEATURE:
